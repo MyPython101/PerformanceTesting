50762d7cdb8ec2fcb4f495beccec117940f901b4
TrucDev <jackyhuynh87@gmail.com>
2022-03-11 10:32:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ from locust import HttpUser, task, between
 
 class TestCases(HttpUser):
     host = "https://template-blog.herokuapp.com/"
-
-    # we assume someone who is browsing the Locust docs,
-    # generally has a quite long waiting time (between
-    # 10 and 300 seconds), since there's a bunch of text
-    # on each page
     wait_time = between(1, 300)
-
 
     def on_start(self):
         self.client.post("/login", json={"Email": "jackyhuynh87@gmail.com", "Password":"1234"})
